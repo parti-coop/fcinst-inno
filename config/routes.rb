ba88ed95f69bdfd73a8da048cfabda9f950ebd49
qus <qus1225@gmail.com>
2017-02-06 17:34:01 +0900

    메인화면이 추가됩니다

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
+  get 'about', to: 'pages#about', as: :about
+
+  resources :interviews
+  resources :insights
+
   namespace :admin do
     resources :interviews
   end
