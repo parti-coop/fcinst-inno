93cd40e48b1f7017e8642af89180975af0cb0bd8
qus <qus1225@gmail.com>
2017-02-06 15:53:48 +0900

    sidekiq을 사용하는 코드를 지웁니다

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,5 @@ module FcinstInno
     config.time_zone = 'Asia/Seoul'
     config.i18n.available_locales = [:en, :ko]
     config.i18n.default_locale = :ko
-    config.active_job.queue_adapter = ((Rails.env.test? or  Rails.env.development?) ? :inline : :sidekiq)
   end
 end
