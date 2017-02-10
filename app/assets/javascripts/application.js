6bcb651cdba83a5b0c2deaa353cfbb39263ddbe8
qus <qus1225@gmail.com>
2017-02-10 16:53:28 +0900

    seo데이터가 추가되고, 소개 및 사이트 공유를 할 수 있습니다

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,76 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require blocs
+//= require jssocials
+//= require kakao
+
+Kakao.init('929bb882ab44bacf4c2c27a9de2156c6');
+
+$.is_blank = function (obj) {
+  if (!obj || $.trim(obj) === "") return true;
+  if (obj.length && obj.length > 0) return false;
+
+  for (var prop in obj) if (obj[prop]) return false;
+  return true;
+}
+
+$.inno_apply = function($base, query, callback) {
+  $.each($base.find(query), function(i, elm){
+    callback(elm);
+  });
+}
+
+var inno_prepare = function($base) {
+  $.inno_apply($base, '[data-action="inno-share"]', function(elm) {
+    var $elm = $(elm);
+
+    var url = $elm.data('share-url');
+    var sitename = $elm.data('share-sitename');
+    var text = $elm.data('share-text');
+    var share = $elm.data('share-provider');
+    var logo = $elm.data('share-logo');
+    var css = $elm.data('share-css');
+    if ($.is_blank(share)) return;
+    var image_url = $elm.data('share-image');
+    var image_width = $elm.data('share-image-width');
+    var image_height = $elm.data('share-image-height');
+
+    switch(share) {
+    case 'kakao-link':
+      Kakao.Link.createTalkLinkButton({
+        container: elm,
+        label: text,
+        image: {
+          src: image_url,
+          width: image_width,
+          height: image_height
+        },
+        webLink: {
+          text: sitename + '에서 보기',
+          url: url
+        }
+      });
+    break
+    case 'kakao-story':
+      $elm.on('click', function(e) {
+        Kakao.Story.share({
+          url: url,
+          text: text
+        });
+      });
+    break
+    default:
+      $elm.jsSocials({
+        showCount: false,
+        showLabel: false,
+        shares: [{share: share, logo: logo, css: css}],
+        text: text,
+        url: url
+      });
+    }
+  });
+};
+
+$(function(){
+  inno_prepare($('body'));
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,45 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :prepare_meta_tags, if: "request.get?"
 
   def errors_to_flash(model)
     flash[:notice] = model.errors.full_messages.join('<br>').html_safe
+  end
+
+  def prepare_meta_tags(options={})
+    set_meta_tags build_meta_options(options)
+  end
+
+  def build_meta_options(options)
+    site_name = "혁신의 편"
+    title = options[:title] || "당신은 혁신의 편입니까?"
+    image = options[:image] || view_context.image_url('seo.png')
+    url = options[:url] || root_url
+    description = options[:description] || "혁신을 키우기 위한 정책 제안 프로젝트"
+
+    {
+      title:       title,
+      reverse:     true,
+      image:       image,
+      description: description,
+      keywords:    "정책, 혁신, 여시재, innovator, 혁신가, 사회적기업, 스타트업, 투자지원기관, 정책연구, 소셜벤처",
+      canonical:   url,
+      twitter: {
+        site_name: site_name,
+        site: '@parti_xyz',
+        card: 'summary',
+        title: title,
+        description: description,
+        image: image
+      },
+      og: {
+        url: url,
+        site_name: site_name,
+        title: title,
+        image: image,
+        description: description,
+        type: 'website'
+      }
+    }
   end
 end
