7e58c1b30a4592d376ac665fa04b257ad6f3d934
qus <qus1225@gmail.com>
2017-02-10 17:44:16 +0900

    인터뷰상세페이지 소셜공유가 추가됩니다

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -6,5 +6,17 @@ class InterviewsController < ApplicationController
 
   def show
     @interview = Interview.find(params[:id])
+    reset_meta_tags_for_show(@interview)
+  end
+
+  private
+
+  def reset_meta_tags_for_show(interview)
+    prepare_meta_tags({
+      title: "당신은 혁신의 편입니까? | #{@interview.name}님의 인터뷰 ",
+      description: @interview.summary,
+      image: @interview.image.url,
+      url: request.original_url}
+    )
   end
 end
