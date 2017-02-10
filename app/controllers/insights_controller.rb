d5a66a9a751b299f752fa66a330b9be293fa54d5
qus <qus1225@gmail.com>
2017-02-11 00:24:06 +0900

    seo공유문구가 추가되고, 인사이트 공유영역이 추가됩니다

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -1,2 +1,15 @@
 class InsightsController < ApplicationController
+  def index
+    reset_meta_tags_for_index
+  end
+
+  private
+
+  def reset_meta_tags_for_index
+    prepare_meta_tags({
+      title: "당신은 혁신의 편입니까? | 인사이트",
+      description: "앞장 서서 혁신의 편에 있는 사람들의 이야기에서 발견한 인사이트를 공유합니다. ",
+      url: request.original_url}
+    )
+  end
 end
--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,6 @@
 class InterviewsController < ApplicationController
   def index
+    reset_meta_tags_for_index
     @interviews = []
     6.times { @interviews.push(Interview.first) }
   end
@@ -11,9 +12,17 @@ class InterviewsController < ApplicationController
 
   private
 
+  def reset_meta_tags_for_index
+    prepare_meta_tags({
+      title: "당신은 혁신의 편입니까? | 혁신가들의 인터뷰",
+      description: "사회 혁신의 맨 앞이기에 누구보다 먼저 겪는 어려움, 어떻게 해결할 수 있을까요? ",
+      url: request.original_url}
+    )
+  end
+
   def reset_meta_tags_for_show(interview)
     prepare_meta_tags({
-      title: "당신은 혁신의 편입니까? | #{@interview.name}님의 인터뷰 ",
+      title: "당신은 혁신의 편입니까? | #{@interview.name}님의 인터뷰",
       description: @interview.summary,
       image: @interview.image.url,
       url: request.original_url}
