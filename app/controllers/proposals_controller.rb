4a39bd418e34fb61ba21e280e3444693a52a6d35
qus <qus1225@gmail.com>
2017-02-11 00:05:54 +0900

    제안에 공감할 수 있습니다

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -2,14 +2,19 @@ class ProposalsController < ApplicationController
   include ApplicationHelper
 
   def upvote
-    @proposal = Proposal.find(params[:id])
-    return if upvoted?(@proposal)
+    if request.format == 'text/javascript'
+      @proposal = Proposal.find(params[:id])
+      return if upvoted?(@proposal)
 
-    if @proposal.present?
-      @proposal.upvotes_count += 1
-      mark_upvoted(@proposal) if @proposal.save
+      if @proposal.present?
+        @proposal.upvotes_count += 1
+        mark_upvoted(@proposal) if @proposal.save
+      end
     end
 
-    redirect_to :back
+    respond_to do |format|
+      format.js
+      format.any { redirect_to root_path }
+    end
   end
 end
