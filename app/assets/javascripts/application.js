a87c017346405ee2e8f5108e53b1ab7613e596ec
qus <qus1225@gmail.com>
2017-02-11 00:54:45 +0900

    인사이트에 디스커스가 추가됩니다

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -75,6 +75,19 @@ var inno_prepare = function($base) {
   });
 };
 
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+
+
+
+
 $(function(){
   inno_prepare($('body'));
   $('#video-background').YTPlayer({
@@ -82,4 +95,9 @@ $(function(){
       videoId: 'W0LHTWG-UmQ',
       mute: false
   });
+
+  var d = document, s = d.createElement('script');
+  s.src = '//innovators-1.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
 });
