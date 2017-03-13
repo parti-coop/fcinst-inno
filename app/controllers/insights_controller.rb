7af8e6682cad7b1d290a1d02b27bcfc72bf342a9
qus <qus1225@gmail.com>
2017-03-13 17:07:05 +0900

    인사이트 리스트가 추가됩니다

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -5,8 +5,8 @@ class InsightsController < ApplicationController
   end
 
   def show
-    reset_meta_tags_for_index
     @insight = Insight.find(params[:id])
+    reset_meta_tags_for_show(@insight)
   end
 
   private
@@ -15,6 +15,15 @@ class InsightsController < ApplicationController
     prepare_meta_tags({
       title: "당신은 혁신의 편입니까? | 인사이트",
       description: "앞장 서서 혁신의 편에 있는 사람들의 이야기에서 발견한 인사이트를 공유합니다. ",
+      url: request.original_url}
+    )
+  end
+
+  def reset_meta_tags_for_show(insight)
+    prepare_meta_tags({
+      title: "당신은 혁신의 편입니까? | #{@insight.title}",
+      description: @insight.summary,
+      image: @insight.image.url,
       url: request.original_url}
     )
   end
