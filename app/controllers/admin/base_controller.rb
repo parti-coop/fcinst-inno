f19c9e4e805f5f5d7c0bcbfdada4a8abfcb46e40
qus <qus1225@gmail.com>
2017-04-05 14:23:23 +0900

    관리자 로그인정보가 환경변수로 바뀝니다

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,5 @@
 module Admin
   class BaseController < ApplicationController
-    http_basic_authenticate_with name: "admin", password: "1234"
+    http_basic_authenticate_with name: ENV['ADMIN_ID'], password: ENV['ADMIN_PASSWORD']
   end
 end
