1e70157e0fe1ea91d56bb96b1ec6658ffe13088f
dali <dalikim@parti.xyz>
2017-04-07 11:33:10 +0900

    컨퍼런스 페이지 추가합니다

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about', to: 'pages#about', as: :about
+  get 'conference', to: 'pages#conference', as: :conference
 
   post 'proposals/:id/upvote', to: 'proposals#upvote', as: :upvote_proposal
 
