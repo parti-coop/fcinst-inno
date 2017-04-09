e7c02ff65b3fdbb241a9e333fe7fd6dc76157320
dali <dalikim@parti.xyz>
2017-04-09 14:10:13 +0900

    컨퍼런스 참가 페이지에 참가링크와 소셜공유 설정을 합니다

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,16 @@
 class PagesController < ApplicationController
   def about
-    reset_meta_tags_for_about
-  end
-
-  private
-
-  def reset_meta_tags_for_about
     prepare_meta_tags({
       title: "당신은 혁신의 편입니까? | 프로젝트 소개",
       description: "혁신가들에게 묻고 전문가들에게 도움받아 새로운 사회시스템을 위한 정책을 만듭니다.",
+      url: request.original_url}
+    )
+  end
+
+  def conference
+    prepare_meta_tags({
+      title: "컨퍼런스 | 당신은 혁신의 편입니까?",
+      description: "혁신 정책 컨퍼런스에 초대합니다.",
       url: request.original_url}
     )
   end
