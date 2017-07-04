de780da54d21c0b033954bacad9dbacef3e8b85f
dali <dalikim@parti.xyz>
2017-07-04 13:41:35 +0900

    동영상 교체

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -86,7 +86,8 @@ $(function(){
   inno_prepare($('body'));
   $('#video-background').YTPlayer({
       fitToBackground: true,
-      videoId: 'MCWy_z8p17k'
+      videoId: 'qrictq2NkCU'
+      //videoId: 'MCWy_z8p17k'
       //videoId: 'fD1ReY_HqNM'
   });
 
